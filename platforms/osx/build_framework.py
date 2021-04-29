7e1a8f852d8af3dda96a248c6d30b5873f24dacd
Giles Payne <gilespayne@gmail.com>
2021-04-29 14:24:27 +0900

    Add build performance tests option to iOS/macOS builds

--- platforms/osx/build_framework.py
+++ platforms/osx/build_framework.py
@@ -78,6 +78,7 @@ if __name__ == "__main__":
     parser.add_argument('--run_tests', default=False, dest='run_tests', action='store_true', help='Run tests')
     parser.add_argument('--build_docs', default=False, dest='build_docs', action='store_true', help='Build docs')
     parser.add_argument('--disable-swift', default=False, dest='swiftdisabled', action='store_true', help='Disable building of Swift extensions')
+    parser.add_argument('--build_perf_tests', default=False, dest='build_perf_tests', action='store_true', help='Build performance tests')
 
     args, unknown_args = parser.parse_known_args()
     if unknown_args:
@@ -128,5 +129,5 @@ if __name__ == "__main__":
     if catalyst_archs:
         targets.append((catalyst_archs, "Catalyst")),
 
-    b = OSXBuilder(args.opencv, args.contrib, args.dynamic, True, args.without, args.disable, args.enablenonfree, targets, args.debug, args.debug_info, args.framework_name, args.run_tests, args.build_docs, args.swiftdisabled)
+    b = OSXBuilder(args.opencv, args.contrib, args.dynamic, True, args.without, args.disable, args.enablenonfree, targets, args.debug, args.debug_info, args.framework_name, args.run_tests, args.build_docs, args.swiftdisabled, args.build_perf_tests)
     b.build(args.out)
